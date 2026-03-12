95811f839dc3095d578448fcad61da3192597c04
Jacek Kozakowski <jk.kozakowski@gmail.com>
2026-03-12 14:46:55 +0100

    feat: Add adding documents to db
    - Moved checking if db exists to vector_storage
    - Add checking if file already in db

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 from rag.loader import load_file
 from rag.splitter import split_documents
 from rag.vector_storage import save_to_db, search
-import os
 
 load_dotenv()
 
@@ -10,10 +9,7 @@ docs = load_file("test_data/systemy_operacyjne.pdf")
 
 chunks = split_documents(docs)
 
-if os.path.exists("./chroma_db"):
-    print("Baza już istnieje, pomijam wektoryzację")
-else:
-    save_to_db(chunks)
+save_to_db(chunks, source_file="systemy_operacyjne.pdf")
 
 query = "jakie algorytmy?"
 results = search(query, k=3)
--- rag/vector_storage.py
+++ rag/vector_storage.py
@@ -10,19 +10,43 @@ EMBEDDING_MODEL = "text-embedding-3-small"
 def get_embeddings():
     return OpenAIEmbeddings(
         model=EMBEDDING_MODEL,
-        openai_api_type=os.getenv("OPENAI_API_KEY")
+        openai_api_key=os.getenv("OPENAI_API_KEY")
     )
 
 
-def save_to_db(chunks):
+def get_indexed_files() -> set:
+    if not os.path.exists(CHROMA_PATH):
+        return set()
+    db = load_db()
+    results = db.get()
+    sources = {m.get("source") for m in results["metadatas"] if m.get("source")}
+    return sources
+
+def save_to_db(chunks, source_file: str = None):
+    indexed = get_indexed_files()
+    print(f"DEBUG indexed files: {indexed}")
+    if source_file and source_file in indexed:
+        print(f"Source file {source_file} already indexed, skipping")
+        return
+
+    if source_file:
+        for chunk in chunks:
+            chunk.metadata["source"] = source_file
+
     embeddings = get_embeddings()
-    db = Chroma.from_documents(
-        chunks,
-        embeddings,
-        persist_directory=CHROMA_PATH,
-        collection_metadata={"hnsw:space": "cosine"}
-    )
-    print(f"Saved {len(chunks)} chunks to Chroma")
+    if os.path.exists(CHROMA_PATH):
+        db = load_db()
+        db.add_documents(chunks)
+        print(f"Added {len(chunks)} chunks to existing Chroma DB")
+        return db
+    else:
+        db = Chroma.from_documents(
+            chunks,
+            embeddings,
+            persist_directory=CHROMA_PATH,
+            collection_metadata={"hnsw:space": "cosine"}
+        )
+        print(f"Saved {len(chunks)} chunks to Chroma")
     return db
 
 def load_db():
